e33a118e508973657d5665155695f65c0566f1ab
lihua <lihua@163.com>
2020-04-16 09:12:05 -0400

    compile CheckDevAddrConflict

--- go/as/external/api/device.pb.gw.go
+++ go/as/external/api/device.pb.gw.go
@@ -858,18 +858,15 @@ func local_request_DeviceService_GetRandomDevAddr_0(ctx context.Context, marshal
 
 }
 
-var (
-	filter_DeviceService_CheckDevAddrConflict_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
-)
-
 func request_DeviceService_CheckDevAddrConflict_0(ctx context.Context, marshaler runtime.Marshaler, client DeviceServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq DevAddrList
 	var metadata runtime.ServerMetadata
 
-	if err := req.ParseForm(); err != nil {
-		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
 	}
-	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_DeviceService_CheckDevAddrConflict_0); err != nil {
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
@@ -882,7 +879,11 @@ func local_request_DeviceService_CheckDevAddrConflict_0(ctx context.Context, mar
 	var protoReq DevAddrList
 	var metadata runtime.ServerMetadata
 
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_DeviceService_CheckDevAddrConflict_0); err != nil {
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
